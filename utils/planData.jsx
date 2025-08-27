e6cef76eba4990c7f9d10d2040b77a8a254be0cf
daksh saini <157453970+iamdakshsainii@users.noreply.github.com>
2025-08-28 02:03:46 +0530

    planData

--- utils/planData.jsx
+++ utils/planData.jsx
@@ -1,81 +1,31 @@
-<<<<<<< HEAD
 export default [
-    {
-        id: 1,
-        name: 'Free',
-        cost: 0,
-        offering: [
-            { value: '✔️ Create 3 Free Mock Interviews' },
-            { value: '✔️ Unlimited Retake Interviews' },
-            { value: '❌ Practice Questions' },
-            { value: '❌ Email Support' },
-            { value: '❌ Custom Interview Types' },
-            { value: '❌ Access to Exclusive Content directly' },
-        ]
-    },
-    {
-        id: 2,
-        name: 'Monthly',
-        cost: 7.99,
-        paymentLink: 'https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV',
-        offering: [
-            { value: '✔️ Create 12 Free Mock Interviews' },
-            { value: '✔️ Unlimited Retake Interviews' },
-            { value: '✔️ Practice Questions' },
-            { value: '✔️ Email Support (Priority)' },
-            { value: '✔️ Custom Mock Interview Types' },
-            { value: '✔️ Access to Exclusive Content' },
-            { value: '✔️ Downloadable PDFs' },
-            { value: '✔️ Progress Tracker' },
-        ]
-    }
-]
-=======
-export default[
-    {
-        id:1,
-        name:'Free',
-        cost:0,
-        offering:[
-            {
-                value:'✔️ Create 3 Free Mock Interview'
-            },
-            {
-                value:'✔️ Unlimited Retake Interview'
-            },
-            {
-                value:'❌ Practice Question'
-            },
-            {
-                value:'❌ Tubeguruji.Com Exlcusive App Access'
-            },
-            {
-                value:'❌ Email Support'
-            },
-        ]
-    },
-    {
-        id:1,
-        name:'Monthly',
-        cost:7.99,
-        paymentLink:'https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV',
-        offering:[
-            {
-                value:'✔️ Create 3 Free Mock Interview'
-            },
-            {
-                value:'✔️ Unlimited Retake Interview'
-            },
-            {
-                value:'✔️ Practice Question'
-            },
-            {
-                value:'✔️ Tubeguruji.Com Exlcusive App Access'
-            },
-            {
-                value:'✔️ Email Support'
-            }
-        ]
-    }
-]
->>>>>>> e0dc322c62330917ba1f03f5464ccd9992454974
+  {
+    id: 1,
+    name: 'Free',
+    cost: 0,
+    offering: [
+      { value: '✔️ Create 3 Free Mock Interviews' },
+      { value: '✔️ Unlimited Retake Interviews' },
+      { value: '❌ Practice Questions' },
+      { value: '❌ Email Support' },
+      { value: '❌ Custom Interview Types' },
+      { value: '❌ Access to Exclusive Content directly' },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Monthly',
+    cost: 7.99,
+    paymentLink: 'https://buy.stripe.com/test_28o9EjbkvgfQ8VOeUV',
+    offering: [
+      { value: '✔️ Create 12 Free Mock Interviews' },
+      { value: '✔️ Unlimited Retake Interviews' },
+      { value: '✔️ Practice Questions' },
+      { value: '✔️ Email Support (Priority)' },
+      { value: '✔️ Custom Mock Interview Types' },
+      { value: '✔️ Access to Exclusive Content' },
+      { value: '✔️ Downloadable PDFs' },
+      { value: '✔️ Progress Tracker' },
+    ],
+  },
+];
