e73b2157295eb03f1ac97b32e4f012c290133960
daksh saini <157453970+iamdakshsainii@users.noreply.github.com>
2025-08-28 02:00:14 +0530

    Resolved merge conflict in DashboardLayout

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,32 +1,18 @@
 import React from 'react'
 import Header from './_components/Header'
-<<<<<<< HEAD
 import Footer from '@/components/Footer'
-=======
->>>>>>> e0dc322c62330917ba1f03f5464ccd9992454974
 
 function DashboardLayout({children}) {
   return (
     <div>
-        <Header/>
-<<<<<<< HEAD
-        <div className='mx-6 md:mx-20 lg:mx-36'>
+      <Header/>
+      <div className='mx-6 md:mx-20 lg:mx-36'>
         {children}
-        {/* <Footer /> */}
-        </div>
-
-=======
-        <div className='mx-5 md:mx-20 lg:mx-36'>
-        {children}
-        </div>
-       
->>>>>>> e0dc322c62330917ba1f03f5464ccd9992454974
+      </div>
+      {/* Uncomment if you want footer */}
+      {/* <Footer /> */}
     </div>
   )
 }
 
-<<<<<<< HEAD
 export default DashboardLayout
-=======
-export default DashboardLayout
->>>>>>> e0dc322c62330917ba1f03f5464ccd9992454974
